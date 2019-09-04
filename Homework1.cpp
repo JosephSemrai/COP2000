963cab2960470c314527d1ba05a590cc19bbe75a
Joseph Semrai <josephsemrai@gmail.com>
2019-09-03 21:15:43 -0400

    First homework assignment

---
+++ Homework1.cpp
@@ -0,0 +1,34 @@
+// Joseph Semrai
+// Description: Outputs the prices for items that are for sale from Gunter's outlet shop.
+// September 1st, 2019
+// COP2000-20201-1V-M-001
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int climbingBootsPrice = 59;
+    int pulloversPrice = 10;
+    int gaitersPrice = 12;
+    int cleatsPrice = 14;
+
+    cout << "********************************************" << endl;
+    cout << "**          //\\                           **" << endl;
+    cout << "**         //  \\  //\\                     **" << endl;
+    cout << "**        //    \\//  \\      //\\           **" << endl;
+    cout << "**       //           \\    //  \\          **" << endl;
+    cout << "**      //             \\  //    \\         **" << endl;
+    cout << "**     //               \\//      \\        **" << endl;
+    cout << "**    //                          \\       **" << endl;
+    cout << "********************************************" << endl;
+    cout << "** Welcome to Gunter's Mountain Shop      **" << endl;
+    cout << "********************************************" << endl;
+    cout << endl << "Sale Items\t\t\tSale Price" << endl;
+    cout << "Hunter Climbing Boots\t\t$" << climbingBootsPrice << endl;
+    cout << "Fleece Pullovers\t\t$" << pulloversPrice << endl;
+    cout << "Snow Gaiters\t\t\t$" << gaitersPrice << endl;
+    cout << "Traction Cleats\t\t\t$" << cleatsPrice << endl;
+
+    return 0;
+}
